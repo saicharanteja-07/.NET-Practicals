715a7196dc3e58024be5c45a2cce485579fa757f
madhucharan2007 <durgammadhu25@gmail.com>
2026-09-23 10:24:38 +0530

    Added Practical6

---
+++ Practical6/Controllers/ProductDetailsController.cs
@@ -0,0 +1,58 @@
+﻿using Practical6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical6.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> P = GetProduct();
+
+            return View("Product", P);
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Product> P = GetProduct();
+            Product selectedProduct = P.FirstOrDefault(x => x.ProductID == id);
+            return View(selectedProduct);
+        }
+        
+        private List<Product> GetProduct()
+        {
+            List<Product> P = new List<Product>();
+
+            Product p1 = new Product();
+            p1.ProductID = 101;
+            p1.ProductName = "Mobile";
+            p1.Category = "Mobile";
+            p1.Price = 10000;
+            p1.Imageurl = "https://loremflickr.com/150/150/mobile";
+
+            Product p2 = new Product();
+            p2.ProductID = 102;
+            p2.ProductName = "Laptop";
+            p2.Category = "Electronics";
+            p2.Price = 65000;
+            p2.Imageurl = "https://loremflickr.com/150/150/laptop";
+
+            Product p3 = new Product();
+            p3.ProductID = 103;
+            p3.ProductName = "HeadPhones";
+            p3.Category = "Accessories";
+            p3.Price = 1500;
+            p3.Imageurl = "https://loremflickr.com/150/150/headphones";
+
+            P.Add(p1);
+            P.Add(p2);
+            P.Add(p3);
+
+            return P;
+        }
+    }
+}
---
+++ Practical6/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practical6.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Category { get; set; }
+
+        public double Price { get; set; }
+
+        public string Imageurl { get; set; }
+    }
+}
